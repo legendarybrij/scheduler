fa2da2ffcf74de4d7a68683f087248f5c5164f02
Brijesh Patel <Brij@Brijesh-Patels-MacBook-Pro.local>
2019-03-07 16:50:08 -0500

    bonus activity

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,8 +12,12 @@
 
   var database = firebase.database();
 
+  //var connectionsRef = database.ref("/schedule");
+
   console.log(database);
   var nameT = "";
+  
+  
   
 
 $(document).ready(function(){ 
@@ -27,13 +31,13 @@ $("#submit-info").on("click", function(event){
   frequencyT = $("#frequency-time").val().trim();
 
 
-    var tFrequency = frequencyT;
+    
 
     // Time is firstT
-    var firstTime = firstT;
+    
 
     // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    var firstTimeConverted = moment(firstT, "HH:mm").subtract(1, "years");
     console.log(firstTimeConverted);
 
     // Current Time
@@ -45,11 +49,11 @@ $("#submit-info").on("click", function(event){
     console.log("DIFFERENCE IN TIME: " + diffTime);
 
     // Time apart (remainder)
-    var tRemainder = diffTime % tFrequency;
+    var tRemainder = diffTime % frequencyT;
     console.log(tRemainder);
 
     // Minute Until Train
-    var tMinutesTillTrain = tFrequency - tRemainder;
+    var tMinutesTillTrain = frequencyT - tRemainder;
     console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
     // Next Train
@@ -59,7 +63,7 @@ $("#submit-info").on("click", function(event){
 
   //console.log(name);
 
-  database.ref().push({
+  var dataId = database.ref("/schedule").push({
     name: nameT,
     destination: destinationT,
     firstTime: firstT,
@@ -68,6 +72,14 @@ $("#submit-info").on("click", function(event){
     minAway: tMinutesTillTrain
   });
 
+  dataId = dataId.toString().substring(48);
+
+  database.ref("/id").push({
+   deleteId: dataId
+  });
+
+  
+
   $("#train-name").val("");
   $("#destination-name").val("");
   $("#first-time").val("");
@@ -75,15 +87,19 @@ $("#submit-info").on("click", function(event){
 });
 
 
-database.ref().on("child_added", function(snapshot) {
+database.ref("/schedule").on("child_added", function(snapshot) {
     // storing the snapshot.val() in a variable for convenience
     var sv = snapshot.val();
+    
     var newRow = $("<tr>");
     var newCol1 = $("<td>");
     var newCol2 = $("<td>");
     var newCol3 = $("<td>");
     var newCol4 = $("<td>");
     var newCol5 = $("<td>");
+    var update = $("<td>");
+    var remove = $("<td>");
+
 
     console.log("NAME: " +sv.name + " Destination: " +sv.destination +" First-time: "+ sv.firstTime+ " Frequency: "+sv.frequencyTime);
     
@@ -91,26 +107,98 @@ database.ref().on("child_added", function(snapshot) {
     newRow.append(newCol1);
     newCol1.text(sv.name);
     newCol1.addClass("table-success table-hover");
+    newCol1.attr("contenteditable","true")
+
     newRow.append(newCol2);
     newCol2.text(sv.destination);
     newCol2.addClass("table-success table-hover");
+    newCol2.attr("contenteditable","true")
+
     newRow.append(newCol3);
     newCol3.text(sv.frequencyTime);
+    newCol3.attr("contenteditable","true")
     newCol3.addClass("table-success table-hover");
+
     newRow.append(newCol4);
     newCol4.text(sv.nextArrival);
     newCol4.addClass("table-success table-hover");
+
     newRow.append(newCol5);
     newCol5.text(sv.minAway);
     newCol5.addClass("table-success table-hover");
+
+    newRow.append(update);
+    update.text("Update");
+    update.addClass("table-success table-hover btn-success updateEdit");
+
+    newRow.append(remove);
+    remove.text("Remove");
+    remove.attr("id","removeData");
+    /*database.ref("/id").on("child_added", function(snapshot) {
+      var sv = snapshot.val();
+    remove.attr("id",sv.deleteId);
+    });*/
+    remove.addClass("table-danger table-hover btn-danger removeData");
+    
 
     // Handle the errors
   }, function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
   });
+
+
+  $("#removeData").on("click", function(){
+
+  //  var deleteId = $(".removeData").attr("id");
+   console.log("Calling REmove Method");
+  /* var adaRef = firebase.database().ref('schedule/id/'+deleteId);
+        adaRef.remove()
+        .then(function() {
+          console.log("Remove succeeded.")
+        })
+        .catch(function(error) {
+          console.log("Remove failed: " + error.message)
+        });*/
+
+  });
+
   
 
-  console.log(database);
+
+  
+
+
+  
+
+
+  
+
+  
 
 });
 
+
+
+//To reduce minutes away till 0
+//Until minuterAway>0 count-- every minute = 60000 in setInterval
+//everytime count -- is called get into firebase and update it in each ID whose minuter away are >0
+
+/*function reduceTime() {
+    
+    timeMachine = setInterval(decrease, 1000);
+  }
+
+  function decrease(){
+    
+    if(time>0)
+      { 
+        time--;
+        document.getElementById("minA").innerHTML = "Time Left: " + time;
+      }else{
+
+          game.timesUp();
+
+      }
+    
+      
+  }*/
